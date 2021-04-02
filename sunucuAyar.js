98dce466ae63af7b9eed16a1cc55b7d65c15bdf2
heykirBaby <77734508+heykirBaby@users.noreply.github.com>
2021-04-02 14:34:10 +0300

    Add files via upload

---
+++ sunucuAyar.js
@@ -0,0 +1,13 @@
+const ayarlar = {
+  guildID: '703353938056970322', // sunucu idsi
+  ownerRole:"825831688151498752", // sunucu sahiplerinin rolü
+  teyitsizRolleri:[825831748867850312], // kayıt olmayanlara verieln rol
+  jailRolu:"825831750441107516", // cezalılara verilen rol
+  enAltYetkiliRolu:[826486428023128144], // ilk yetkili permi
+  activity:"sa", // botun durumu
+  status:"dnd",
+  invitelink:"discord.gg/alfea", //davet linki
+  symbol:"◭ ", // tag
+  voicechannel:"826757614011547649" // botun gireceği ses kanalı
+};
+module.exports = ayarlar;
